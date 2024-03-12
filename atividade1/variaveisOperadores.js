083534105c1e4f50aadd18e00f30b8682ae368cf
Gabrielsandres <gabrielsandresrocha@gmail.com>
2024-03-12 11:54:34 -0300

    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,26 @@
+let adicao1 = 5
+let adicao2 = 5
+let resultadoAdicao = adicao1 + adicao2
+
+let sub1 = 5
+let sub2 = 5
+let resultadoSub = sub1 - sub2
+
+let mult1 = 5
+let mult2 = 5
+let resultadoMult = mult1 * mult2
+
+let div1 = 5
+let div2 = 5
+let resultadoDiv = div1 / div2
+
+
+
+
+console.log ("\nResultado da adição entre " + adicao1 + " e " + adicao2 + " é igual a: " + resultadoAdicao)
+
+console.log ("\nResultado da subtração entre " + sub1 + " e " + sub2 + " é igual a: " + resultadoSub)
+
+console.log ("\nResultado da multiplicação entre " + mult1 + " e " + mult2 + " é igual a: " + resultadoMult)
+
+console.log ("\nResultado da divisão entre " + div1 + " e " + div2 + " é igual a: " + resultadoDiv)
