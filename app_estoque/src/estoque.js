49e7e7dfd700b16c64b2b12900c118356b61a51a
GABRIEL SANDRES ROCHA DE SOUZA <gabriel.souza@a.ucb.br>
2024-04-11 11:17:50 -0300

    Estoque

---
+++ app_estoque/src/estoque.js
@@ -0,0 +1,33 @@
+let produtos = []
+
+function criarProduto(id, nome, qtd) {
+    
+    let p = {
+
+        id:id,
+        nome: nome,
+        qtd: qtd
+    };
+    return p;
+}
+
+function adicionarProduto(p) {
+    produtos.push(p)
+}
+
+function listarProdutos() {
+    return produtos
+}
+
+function removerProduto(id) {
+    produtos = produtos.filter(function (p) {
+        return p.id != id;
+    })
+}
+
+module.exports = {
+    criarProduto,
+    adicionarProduto,
+    listarProdutos,
+    removerProduto
+}
